855bebae052eefacb0e004b509f0b09241492dea
Matthew Ascone <asconem@gmail.com>
2020-07-22 16:54:09 -0400

    Modify UserApplication class to include RibbonClient

--- user/src/main/java/UserApplication.java
+++ user/src/main/java/UserApplication.java
@@ -2,17 +2,22 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @RestController
+@RibbonClient(name = "say-hello", configuration = SayHelloConfiguration.class)
 public class UserApplication {
 
+    @LoadBalanced
     @Bean
     RestTemplate restTemplate(){
         return new RestTemplate();
@@ -23,7 +28,7 @@ public class UserApplication {
 
     @RequestMapping("/hi")
     public String hi(@RequestParam(value="name", defaultValue="Artaban") String name) {
-        String greeting = this.restTemplate.getForObject("http://localhost:8090/greeting", String.class);
+        String greeting = this.restTemplate.getForObject("http://say-hello/greeting", String.class);
         return String.format("%s, %s!", greeting, name);
     }
 
