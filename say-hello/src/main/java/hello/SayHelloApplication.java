ec33858a837d04e00e71e46ff5c911a59423d6fc
Matthew Ascone <asconem@gmail.com>
2020-07-22 17:00:28 -0400

    Refactor SayHelloApplication into hello package

--- say-hello/src/main/java/hello/SayHelloApplication.java
+++ say-hello/src/main/java/hello/SayHelloApplication.java
@@ -1,3 +1,5 @@
+package hello;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
